6ade55a5fd05f6d65d17d58b6ef00c2fa3e6f2e7
Siko Chang <sikochang@126.com>
2018-01-21 22:18:50 +0800

    modify delivery-new-order html

--- jmanager-app/src/app/delivery/delivery.module.ts
+++ jmanager-app/src/app/delivery/delivery.module.ts
@@ -5,8 +5,13 @@ import {RouterModule, Routes} from '@angular/router';
 import {DeliveryBoardComponent} from './delivery-board/delivery-board.component';
 import { DeliveryNewOrderComponent } from './delivery-new-order/delivery-new-order.component';
 
+import {CalendarModule} from 'primeng/calendar';
+import {CheckboxModule} from 'primeng/checkbox';
+import {DropdownModule} from 'primeng/dropdown';
 import {ButtonModule} from 'primeng/button';
 import {CardModule} from '../../../node_modules/primeng/components/card/card';
+import {FieldsetModule} from 'primeng/fieldset';
+import {PanelModule} from 'primeng/panel';
 
 
 const deliveryRoutes: Routes = [
@@ -18,8 +23,13 @@ const deliveryRoutes: Routes = [
   imports: [
     CommonModule,
     RouterModule.forChild(deliveryRoutes),
+    CalendarModule,
+    CheckboxModule,
+    DropdownModule,
     ButtonModule,
-    CardModule
+    CardModule,
+    FieldsetModule,
+    PanelModule
   ],
   declarations: [DeliveryBoardComponent, DeliveryNewOrderComponent]
 })
