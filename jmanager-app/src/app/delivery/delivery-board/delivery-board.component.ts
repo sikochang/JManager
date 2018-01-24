c1f3bb28571f83a2d7a8c8697f7888c04b4f06e6
Siko Chang <sikochang@126.com>
2018-01-24 22:30:13 +0800

    modify delivery-board html and ts

--- jmanager-app/src/app/delivery/delivery-board/delivery-board.component.ts
+++ jmanager-app/src/app/delivery/delivery-board/delivery-board.component.ts
@@ -6,10 +6,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./delivery-board.component.scss']
 })
 export class DeliveryBoardComponent implements OnInit {
+  private toBePackedOrderCounts: number;
+  private toBeDeliveredOrderCounts: number;
+  private toBeClosedOrderCounts: number;
 
   constructor() { }
 
   ngOnInit() {
+    this.toBePackedOrderCounts = 2;
+    this.toBeDeliveredOrderCounts = 3;
+    this.toBeClosedOrderCounts = 4;
   }
 
 }
